023b6fc837195812984cc70bf1dc307378f06ed7
github <2335287299@qq.com>
2021-11-02 20:59:49 +0800

    添加异步发送邮箱的功能

--- src/main/java/ruangong/our_land/Ruangong4Application.java
+++ src/main/java/ruangong/our_land/Ruangong4Application.java
@@ -4,8 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAspectJAutoProxy
+@EnableAsync
 @SpringBootApplication
 @MapperScan("ruangong.our_land.mapper")
 public class Ruangong4Application {
