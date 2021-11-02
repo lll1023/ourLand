9c63fbb08f42584ef12b1d810b67dc98e8742537
github <2335287299@qq.com>
2021-11-02 17:27:10 +0800

    解决跨域

---
+++ src/main/java/ruangong/our_land/config/WebConfig.java
@@ -0,0 +1,23 @@
+package ruangong.our_land.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author: Lsutin
+ * @Date: 2021/11/2 17:21
+ * @describe:
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET","POST","PUT","DELETE","HEAD")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
