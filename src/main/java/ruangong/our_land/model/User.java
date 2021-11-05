a8f59777ec58362970722cf642e2fefd7b3c61e8
3219005447 <234@234>
2021-11-05 20:37:08 +0800

    编写用户类

--- src/main/java/ruangong/our_land/model/User.java
+++ src/main/java/ruangong/our_land/model/User.java
@@ -1,17 +1,16 @@
 package ruangong.our_land.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+
 /**
  * @Author: LianWL
  * @Date: 2021/11/5 14:23
  * @Description: 用户类
  */
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
-import java.util.ArrayList;
 
 @Data
 @AllArgsConstructor
@@ -24,6 +23,4 @@ public class User {
     private String email;
     private ArrayList<Integer> spirits_bag; //精灵背包
     private ArrayList<Integer> props_bag; //道具背包
-
-
 }
