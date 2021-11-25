69f9d5bfef49d0b7458428581c843c27c16bbe0f
hwy <1090898297@qq.com>
2021-11-25 16:55:14 +0800

    feat:新增用户注册、登录功能

--- src/main/java/ruangong/our_land/service/UserService.java
+++ src/main/java/ruangong/our_land/service/UserService.java
@@ -1,9 +1,53 @@
 package ruangong.our_land.service;
 
+import ruangong.our_land.model.user.User;
+import ruangong.our_land.model.user.UserProp;
+import ruangong.our_land.model.user.UserSpirit;
+
+import java.util.List;
+
 /**
  * @Author: Lsutin
  * @Date: 2021/11/2 14:36
  * @describe:
  */
 public interface UserService {
+
+    /**
+     * 根据用户名获取用户实体
+     * @param uName 用户名
+     * @return
+     */
+    User findByName(String uName);
+
+    /**
+     * 插入用户
+     * @param uName 用户名
+     * @param uPwd 用户密码
+     * @return
+     */
+    Integer insertUser(String uName, String uPwd);
+
+    /**
+     * 根据用户id获取拥有道具
+     * @param uId 用户id
+     * @return
+     */
+    List<UserProp> findProp(Integer uId);
+
+    /**
+     * 根据用户id获取拥有精灵
+     * @param uId 用户id
+     * @return
+     */
+    List<UserSpirit> findSpirit(Integer uId);
+
+    /**
+     * 用户添加精灵
+     * @param uId 用户id
+     * @param sId 精灵id
+     * @return
+     */
+    Integer insertUserSpirit(Integer uId, Integer sId);
+
 }
