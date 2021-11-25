70c174bbf6ede9843d9f1ed10b95ccf5f649a227
TomQuanCode <1960949493@qq.com>
2021-11-25 11:17:51 +0800

    精灵等级计算、精灵血量计算

--- src/main/java/ruangong/our_land/model/spirit/Spirit.java
+++ src/main/java/ruangong/our_land/model/spirit/Spirit.java
@@ -2,16 +2,16 @@ package ruangong.our_land.model.spirit;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
 import ruangong.our_land.model.helper.ObjectHelper;
-import ruangong.our_land.model.spirit.monster.Monster;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 
@@ -108,7 +108,6 @@ public abstract class Spirit {
     protected abstract Skill[] initSkills();
 
 
-
     @Deprecated
     /*public Skill getSkills(String skillName) {
         String skill = ObjectHelper.requireNonNull(skillName, "skillName");
@@ -121,16 +120,6 @@ public abstract class Spirit {
         return skillMap.get(skill);
     }*/
 
-
-
-    /**
-     * 升级，使level+1
-     */
-    protected void levelUp() {
-        if (this instanceof Monster && this.level < MAX_LEVEL) {
-            this.level++;
-        }
-    }
 
     public void setBlood(int blood) {
         this.blood = ObjectHelper.verifyNonZeroPositive(blood, "blood");
@@ -149,6 +138,50 @@ public abstract class Spirit {
     }
 
     /**
+     * 连接数据库
+     *
+     * @return Connection对象
+     * @throws SQLException
+     * @throws java.lang.ClassNotFoundException
+     */
+    public Connection getConnection() throws SQLException, java.lang.ClassNotFoundException {
+        log.info("连接数据库");
+        String url = "jdbc:mysql://39.99.140.114:3306/our_land";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String username = "root";
+        String password = "whibin";
+        Connection con = DriverManager.getConnection(url, username, password);
+        return con;
+    }
+
+    /**
+     * 根据用户经验值获取当前精灵等级
+     * @param exp 用户经验值
+     * @return 精灵当前等级
+     */
+    public int getLevel(int exp) {
+        int level;
+        if (exp >= 10000) {
+            level = MAX_LEVEL;
+        } else {
+            level = exp / 100;
+        }
+        return level;
+    }
+
+    /**
+     * 获取当前精灵血量
+     * @param blood 精灵原血量
+     * @param exp 用户经验值
+     * @return 当前精灵血量
+     */
+    public int getBlood(int blood, int exp) {
+        int curLevel = getLevel(exp);
+        int curBlood = (int) (blood + (curLevel - 1) * 0.1);
+        return curBlood;
+    }
+
+    /**
      * 技能类
      */
     @Data
@@ -158,18 +191,15 @@ public abstract class Spirit {
          * 技能名
          */
         @NonNull
-        @Getter
         String name;
         /**
          * 技能描述
          */
         @NonNull
-        @Getter
         String description;
         /**
          * 技能使用次数
          */
-        @Getter
         int times = 0;
 
         /**
@@ -178,13 +208,13 @@ public abstract class Spirit {
          * 提升型：提升精灵属性（攻击、防御、速度）
          */
         @NonNull
-        @Getter
+
         String type;
 
         /**
          * 技能伤害（若技能为伤害型），用于精灵对战
          */
-        @Getter
+
         int hurt;
 
         //提升型技能的构造方法
@@ -202,88 +232,10 @@ public abstract class Spirit {
             this.hurt = hurt;
         }
 
-        /**
-         * 技能效果：
-         * 伤害型：更新技能伤害值（用于对战）
-         * 提升型：更新精灵属性（仅限于对战）
-         */
-        public void skillEffect() {
-            log.info("技能效果实现");
-            if (this.type.equals("伤害型")) {
-                //当前伤害值 = 伤害值 * 攻击力
-                this.hurt = this.hurt * attack;
-            } else if (this.type.equals("提升型")) {
-                //通过判断技能描述，来判断是提升攻击力、防御力还是速度
-                //开始为简单起见，假设属性提升按+1的方式提升
-                if (this.description.contains("攻击力")) {
-                    attack = attack + 1;
-                } else if (this.description.contains("防御力")) {
-                    defence = defence + 1;
-                } else if (this.description.contains("速度")) {
-                    speed = speed + 1;
-                }
-            }
-        }
 
         public void setTimes(int times) {
             this.times = ObjectHelper.verifyNonZeroPositive(times, "times");
         }
 
-        /**
-         * 连接数据库
-         * @return Connection对象
-         * @throws SQLException
-         * @throws java.lang.ClassNotFoundException
-         */
-        public Connection getConnection() throws SQLException, java.lang.ClassNotFoundException {
-            log.info("连接数据库");
-            String url = "jdbc:mysql://39.99.140.114:3306/our_land";
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            String username = "root";
-            String password = "whibin";
-            Connection con = DriverManager.getConnection(url, username, password);
-            return con;
-        }
-
-        /**
-         * 展示精灵图鉴
-         *
-         * @return 精灵图鉴信息
-         * @throws SQLException
-         * @throws ClassNotFoundException
-         */
-        public String showAllSpirit() throws SQLException, ClassNotFoundException {
-            log.info("通过SQL查询语句，实现对数据库的查询，返回精灵图鉴信息");
-            StringBuffer buffer = new StringBuffer();
-            Connection con = getConnection();
-            Statement sta = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            ResultSet result;
-            result = sta.executeQuery("SELECT s_id,s_name,s_type,s_blood,s_attack,s_defence,s_speed,s_prop,sk_id,sk_name,sk_intro,sk_times,sk_type\n" +
-                    "FROM spirit,skill\n" +
-                    "WHERE spirit.skill1 = skill.sk_id\n" +
-                    "OR spirit.skill2 = skill.sk_id\n" +
-                    "OR spirit.skill3 = skill.sk_id\n" +
-                    "OR spirit.skill4 = skill.sk_id;");
-            buffer.append("精灵ID        精灵名称        精灵类型      初始血量        攻击力        防御力        速度        属性        技能ID        技能名        技能效果        使用次数        技能类型\n");
-            while (result.next()) {
-                String s_id = result.getString("s_id");
-                String s_name = result.getString("s_name");
-                String s_type = result.getString("s_type");
-                int s_blood = result.getInt("s_blood");
-                int s_attack = result.getInt("s_attack");
-                int s_defence = result.getInt("s_defence");
-                int s_speed = result.getInt("s_speed");
-                String s_prop = result.getString("s_prop");
-                int sk_id = result.getInt("sk_id");
-                String sk_name = result.getString("sk_name");
-                String sk_intro = result.getString("sk_intro");
-                int sk_times = result.getInt("sk_times");
-                String sk_type = result.getString("sk_type");
-                buffer.append("  " + s_id + "           " + s_name + "           " + s_type + "         " + s_blood + "           " + s_attack + "           "
-                        + s_defence + "           " + s_speed + "           " + s_prop + "           " + sk_id + "           " + sk_name + "           " +
-                        sk_intro + "           " + sk_times + "           " + sk_type + "\n");
-            }
-            return buffer.toString();
-        }
     }
 }
