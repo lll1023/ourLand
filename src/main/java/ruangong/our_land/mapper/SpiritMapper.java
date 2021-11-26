324ffbcf2b5b6a6456de31adbedf06e5fe3db17b
hwy <1090898297@qq.com>
2021-11-26 21:18:32 +0800

    feat:新增获取战斗信息接口

--- src/main/java/ruangong/our_land/mapper/SpiritMapper.java
+++ src/main/java/ruangong/our_land/mapper/SpiritMapper.java
@@ -3,9 +3,9 @@ package ruangong.our_land.mapper;
 import org.apache.ibatis.annotations.Param;
 import ruangong.our_land.model.spirit.Spirit;
 import ruangong.our_land.model.spirit.boss.Boss;
+import ruangong.our_land.model.spirit.monster.Monster;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * @Author: Lsutin
@@ -14,6 +14,8 @@ import java.util.HashMap;
  */
 public interface SpiritMapper {
     Boss getSpirit(@Param("s_id") int l_boss);
+
+    Monster getMonster(@Param("s_id") Integer sId);
 
     ArrayList<Spirit.Skill> getSkill(@Param("skill1")int skill1,@Param("skill2")int skill2,@Param("skill3")int skill3,@Param("skill4")int skill4);
 
